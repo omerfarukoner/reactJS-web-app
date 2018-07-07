4cd36c06599fe13d2f9762b81d9c4a090b6ba7ea
omerfarukoner <farukoner61@gmail.com>
2018-07-07 15:12:29 +0300

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,53 @@
+import React from "React";
+import {Link} from 'react-router';
+
+
+
+class Header extends React.Component{
+   constructor(){
+     super();
+     this.state = {
+      isNavOpen:false
+    }
+   }
+  buttonClicked(){
+    this.setState(
+      {
+        isNavOpen: !this.state.isNavOpen
+      }
+    )
+  }
+   
+   render(){
+     console.log("RENDERING");
+     return (
+      <header>
+        <nav className="container navbar navbar-expand-lg navbar-light  justify-content-between">
+          <a className="navbar-brand" href="#">
+            Ömer Faruk ÖNER
+          </a>
+          <button className="navbar-toggler" type="button" onClick={this.buttonClicked.bind(this)}>
+              <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse" style={{display: this.state.isNavOpen ? "block" : "none"}}>
+          <ul className="navbar-nav ">
+            <li className="nav-item ">
+            <Link to="/" activeClassName="active" className="nav-link" href="">Anasayfa</Link>
+            </li>
+            <li className="nav-item ">
+            <Link to="/hakkimizda" activeClassName="active" className="nav-link" href="">Hakkımızda</Link>
+            </li>
+            <li className="nav-item ">
+            <Link to="/iletisim" activeClassName="active" className="nav-link" href="">İletişim</Link>
+            </li>
+           
+          </ul>
+          </div>
+        </nav>
+      </header>
+    )   
+   }
+}
+
+
+export default Header;
