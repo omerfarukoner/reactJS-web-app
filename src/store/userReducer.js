42c6cb9ca557a11f90d91bb5ea87f51f7bd10bf1
omerfarukoner <farukoner61@gmail.com>
2018-07-11 17:24:21 +0300

    Third commit

---
+++ src/store/userReducer.js
@@ -0,0 +1,18 @@
+const USER_INIT = 'USER_INIT';
+
+export function userInit(user = {}){
+    return{
+        type : USER_INIT,
+        payload : user
+    }
+}
+
+export default function userReducer(state ={},action){
+    switch(action.type){
+        case USER_INIT:
+              return Object.assign({},action);
+
+        default:
+              return state;
+    }
+}
